b5a009ff2c8e6109d71771bc41319429cd54cd5a
KristinaMumm <mummkristina@gmail.com>
2024-08-21 23:01:57 +0300

    fix: added missing key in loop

--- app/ui/chat.tsx
+++ app/ui/chat.tsx
@@ -2,9 +2,11 @@ import { CoreMessage } from "ai";
 
 export default function Chat({ messageHistory }: { messageHistory: CoreMessage[] }) {
 
-return (<div>
-{messageHistory.map(function(object, i){
-        return <p key={i}>{object.content.toString()}</p>;
-    })}
-</div>)
-}
+    return (
+        <div>
+          {messageHistory.map((object, index) => (
+            <p key={index}>{object.content.toString()}</p>
+          ))}
+        </div>
+      );
+    }
